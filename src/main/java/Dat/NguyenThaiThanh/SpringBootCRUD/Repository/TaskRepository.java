f7a8bd98723ba446f3274b98cc09fa58bdcac724
nttdat95201 <nttdat95201@gmail.com>
2023-11-04 15:19:47 +0700

    Add annotation @Repository

--- src/main/java/Dat/NguyenThaiThanh/SpringBootCRUD/Repository/TaskRepository.java
+++ src/main/java/Dat/NguyenThaiThanh/SpringBootCRUD/Repository/TaskRepository.java
@@ -2,7 +2,9 @@ package Dat.NguyenThaiThanh.SpringBootCRUD.Repository;
 
 import Dat.NguyenThaiThanh.SpringBootCRUD.Model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
     
 }
